f200f49e55b7ee26f7114bf61222642ff1f26ac6
kyalsinviatick <kyal.sin@viatick.com>
2024-05-13 02:12:24 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -9,37 +9,43 @@ import Budget from './components/Budget';
 
 
 import { AppProvider } from './context/AppContext';
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
+import AllocationForm from './components/AllocationForm';
+
 const App = () => {
     return (
         <AppProvider>
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
-                    <div className='row mt-3'>
-                        {
-                            /* Add Budget component here */
-                        }        
+                <div className='row mt-3'>
+                    <div className='col-sm'>
+                        <Budget />
+                    </div>
 
-                        {
-                            /* Add Remaining component here*/
-                        }        
+                    <div className='col-sm'>
+                        <Remaining />
+                    </div>
 
-                        {
-                            /* Add ExpenseTotal component here */
-                        }        
-                       
-                        {
-                            /* Add ExpenseList component here */
-                        }         
-
-                        {
-                            /* Add ExpenseItem component here */
-                        }        
-
-                        {
-                            /* Add AllocationForm component here under */
-                        }        
+                    <div className='col-sm'>
+                        <ExpenseTotal />
+                    </div>
 
                 </div>
+                <div className='row mt-3'>
+
+
+                    <div className='col-sm'>
+                        <ExpenseList />
+                    </div>
+
+                </div>
+
+                <div className='col-sm'>
+                    <AllocationForm />
+                </div>
+
             </div>
         </AppProvider>
     );
